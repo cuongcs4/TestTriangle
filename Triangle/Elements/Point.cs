a70012b24e91e70c106c19f8e9828e90c4e405a0
NGUYỄN TẠ PHÚ CƯỜNG <cn82295@gmail.com>
2020-12-29 03:19:36 +0700

    Upload source

---
+++ Triangle/Elements/Point.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Triangle.Elements
+{
+    public class Point
+    {
+        private int _coordinateX;
+        private int _coordinateY;
+
+        public int coordinateX { get => _coordinateX; set => _coordinateX = value; }
+        public int coordinateY { get => _coordinateY; set => _coordinateY = value; }
+        public Point()
+        {
+            coordinateX = 0;
+            coordinateY = 0;
+        }
+        public Point(int coordinateX, int coordinateY)
+        {
+            this.coordinateX = coordinateX;
+            this.coordinateY = coordinateY;
+        }
+        public void toString()
+        {
+            Console.WriteLine("Point value" + " : (" + this.coordinateX + ", " + this.coordinateY + ")");
+        }
+        public double calculeDistance(Point secondPoint)
+        {   
+            double distance;
+            distance = (this.coordinateX - secondPoint.coordinateX) * (this.coordinateX - secondPoint.coordinateX) + (this.coordinateY - secondPoint.coordinateY) * (this.coordinateY - secondPoint.coordinateY);
+            distance = Math.Sqrt(distance);
+            return distance;
+        }
+        public bool isZeroValueDistance(Point secondPoint)
+        {
+            return this.calculeDistance(secondPoint) == 0;
+        }
+        public bool comparerPoint(Point secondPoint)
+        {
+            bool lesPointsSontIdentiques = false;
+
+            if (this.coordinateX == secondPoint.coordinateX && this.coordinateY == secondPoint.coordinateY)
+            {
+                lesPointsSontIdentiques = true;
+            }
+
+            return lesPointsSontIdentiques;
+        }
+    }
+}
