b2fa710a903c42492fa061eaf01a8e601a3236aa
NGUYỄN TẠ PHÚ CƯỜNG <cn82295@gmail.com>
2020-12-29 03:41:35 +0700

    fix code

---
+++ README.md
@@ -0,0 +1,4 @@
+![Travis Build](https://travis-ci.com/adatdat/Triangle.svg?branch=master)
+
+# Triangle
+Basic calculation perimeter and triangular classification. After that, create unit test follow course requirements
---
+++ Triangle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Triangle
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Type value for x and y coordinate: ");
+            var dataInput = Console.ReadLine().Split(' ');
+            int coordinateX = Convert.ToInt32(dataInput[0]);
+            int coordinateY = Convert.ToInt32(dataInput[1]);
+
+            Point testPoint = new Point(coordinateX, coordinateY);
+            testPoint.toString();
+            TriangleModel testTriangle = new TriangleModel();
+            testTriangle.toString();
+        }
+    }
+}
